783b2d89a0cdc74f0a7b70228c5f8c62d6e8262f
telamonian <mklein@jhu.edu>
2015-05-05 00:23:51 -0400

    finished adding test for python bindings. Can run by executing "python testOcudump.py" in the same directory as you built

---
+++ src/cython/testOcudump.py
@@ -0,0 +1,9 @@
+from time import sleep
+from src.cython.ocudump_cython import Ocudump
+
+if __name__=='__main__':
+    o = Ocudump()
+    while True:
+        o.getPose()
+        print("Current pose - pitch %0.2f, yaw %0.2f, roll %0.2f, x %0.2f, y %0.2f, z %0.2f" % tuple(o.pose))
+        sleep(.1)
