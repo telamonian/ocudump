eb5b1bbe5575f6e3d71fa67093ac2edfd9250b2f
telamonian <mklein@jhu.edu>
2015-07-22 18:05:48 -0400

    finished basic setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,40 @@
-from setuptools import setup
+from distutils.spawn import find_executable
 from distutils.sysconfig import get_python_lib
 import glob
-import os
-import sys
+import os,sys
+from setuptools import setup
+
+from utils import option_value
+
+OPTION_CMAKE = option_value("cmake")
+if OPTION_CMAKE is None:
+    OPTION_CMAKE = find_executable("cmake")
+if OPTION_CMAKE is None or not os.path.exists(OPTION_CMAKE):
+    raise DistutilsSetupError(
+        "Failed to find cmake."
+        " Please specify the path to cmake with --cmake parameter.")
+
+def build_extension(self, extension):
+    # Build module
+    cmake_cmd = [
+        OPTION_CMAKE,
+        "-G", self.make_generator,
+    #     "-DQT_QMAKE_EXECUTABLE=%s" % self.qmake_path,
+    #     "-DBUILD_TESTS=%s" % self.build_tests,
+    #     "-DDISABLE_DOCSTRINGS=True",
+    #     "-DCMAKE_BUILD_TYPE=%s" % self.build_type,
+    #     "-DCMAKE_INSTALL_PREFIX=%s" % self.install_dir,
+        module_src_dir
+    ]
+    
+    if sys.platform == 'win32':
+        cmake_cmd.append("-DCMAKE_BUILD_TYPE=%s" % 'Release')
+        cmake_cmd.append("-DCMAKE_DEBUG_POSTFIX=_d")
 
 setup(
     name = "ocudump",
-    package_dir = {'': 'src/cython'},
-    data_files = [(get_python_lib(), glob.glob('src/cython/*.so'))],
+    package_dir = {'': 'build/src/cython'},
+    data_files = [(get_python_lib(), glob.glob('build/src/cython/*.so'))],
     author = 'Max Klein',
     description = 'lightweight library for dumping out tracking data from an Oculus Rift',
     license = 'UIOSL',
---
+++ utils.py
@@ -0,0 +1,18 @@
+from distutils.errors import DistutilsOptionError
+import os,sys
+
+def option_value(name):
+    for index, option in enumerate(sys.argv):
+        if option == '--' + name:
+            if index+1 >= len(sys.argv):
+                raise DistutilsOptionError(
+                    'The option %s requires a value' % option)
+            value = sys.argv[index+1]
+            sys.argv[index:index+2] = []
+            return value
+        if option.startswith('--' + name + '='):
+            value = option[len(name)+3:]
+            sys.argv[index:index+1] = []
+            return value
+    env_val = os.getenv(name.upper().replace('-', '_'))
+    return env_val
