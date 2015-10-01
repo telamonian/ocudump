02fafd8f13649d7c61f64df3d339174605d1bf73
telamonian <mklein@jhu.edu>
2015-10-01 17:44:01 -0400

    can now build and install everything with a single pip command (eg `pip install -e .`). Tested on mac, support has been added for windows but not tested

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
+# aspects of this build system are based on https://github.com/PySide/pyside-setup
+from distutils.errors import DistutilsSetupError
 from distutils.spawn import find_executable
 from distutils.sysconfig import get_python_lib
 import glob
 import os,sys
 from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
+from setuptools.command.install import install
 
-from utils import option_value
+try:
+    import multiprocessing
+    NCPUS = multiprocessing.cpu_count()
+except ImportError:
+    NCPUS = 1
 
-OPTION_CMAKE = option_value("cmake")
+from utils import IsPython64Bit, OptionValue, RunProcess, WrappedMkdir, WrappedMakedirs
+
+thisScriptDir = os.path.dirname(os.path.realpath(__file__))
+
+OPTION_CMAKE = OptionValue("cmake")
 if OPTION_CMAKE is None:
     OPTION_CMAKE = find_executable("cmake")
 if OPTION_CMAKE is None or not os.path.exists(OPTION_CMAKE):
@@ -14,29 +27,107 @@ if OPTION_CMAKE is None or not os.path.exists(OPTION_CMAKE):
         "Failed to find cmake."
         " Please specify the path to cmake with --cmake parameter.")
 
-def build_extension(self, extension):
-    # Build module
-    cmake_cmd = [
-        OPTION_CMAKE,
-        "-G", self.make_generator,
-    #     "-DQT_QMAKE_EXECUTABLE=%s" % self.qmake_path,
-    #     "-DBUILD_TESTS=%s" % self.build_tests,
-    #     "-DDISABLE_DOCSTRINGS=True",
-    #     "-DCMAKE_BUILD_TYPE=%s" % self.build_type,
-    #     "-DCMAKE_INSTALL_PREFIX=%s" % self.install_dir,
-        module_src_dir
-    ]
+class CustomEggInfoCommand(egg_info):
+    '''
+    customized egg_info command class
+    deals with the fact that the build dir containing the cython-built ocudump library might not exist initially
+    '''
+    ocudumpCythonLibDir = os.path.join(thisScriptDir, 'build/src/cython')
     
-    if sys.platform == 'win32':
-        cmake_cmd.append("-DCMAKE_BUILD_TYPE=%s" % 'Release')
-        cmake_cmd.append("-DCMAKE_DEBUG_POSTFIX=_d")
+    def __init__(self, dist, **kw):
+        WrappedMakedirs(self.ocudumpCythonLibDir) 
+        egg_info.__init__(self, dist, **kw)
+
+class CustomSetupCommand:
+    '''
+    customized setup command base class
+    meant to be used in a subclass that also inherits either setuptools.command.install.install or .develop
+    '''
+    cmakeBuildDir = os.path.join(thisScriptDir, 'build')
+    
+    def run(self):
+        self._build_extension()
+        
+    def _build_extension(self):
+        WrappedMkdir(self.cmakeBuildDir)
+        
+        if sys.platform=='win32':
+            if IsPython64Bit():
+                self.make_generator = 'Visual Studio 12 2013 Win64'
+            else:
+                self.make_generator = 'Visual Studio 12 2013'
+        else:
+            self.make_generator = 'Unix Makefiles'
+        
+        # Build module
+        cmake_cmd = [
+            OPTION_CMAKE,
+            "-G", self.make_generator,
+        #     "-DQT_QMAKE_EXECUTABLE=%s" % self.qmake_path,
+        #     "-DBUILD_TESTS=%s" % self.build_tests,
+        #     "-DDISABLE_DOCSTRINGS=True",
+        #     "-DCMAKE_BUILD_TYPE=%s" % self.build_type,
+        #     "-DCMAKE_INSTALL_PREFIX=%s" % self.install_dir,
+            '..'
+        ]
+        
+        make_cmd = [
+            'cmake',
+            '--build',
+            '.',
+            '--',
+            '-j%d' % NCPUS
+        ]
+        
+        if sys.platform == 'win32':
+            cmake_cmd.append("-DCMAKE_BUILD_TYPE=%s" % 'Release')
+            cmake_cmd.append("-DCMAKE_DEBUG_POSTFIX=_d")
+            
+        if RunProcess(cmake_cmd, cwd=self.cmakeBuildDir) != 0:
+            raise DistutilsSetupError("Error setting up ocudump build with cmake")
+        
+        if RunProcess(make_cmd, cwd=self.cmakeBuildDir) != 0:
+            raise DistutilsSetupError("Error make-ing ocudump build")
+            
+    def _writeLeapConfig(self):
+        '''
+        write config file to set path to leap motion packages
+        '''
+        if self.leapSDKEnvVar not in os.environ:
+            raise EnvironmentError('%s%s' % ('You need to set the %s environment variable before installing ocumol, ' % self.leapSDKEnvVar,
+                                             'e.g. you could run `LEAPSDK_DIR=/usr/local/LeapSDK pip install ocumol`'))
+        leapSDKDir = os.environ.get(self.leapSDKEnvVar)
+        with open('ocumol/leapConfig.py', 'w') as f:
+            f.write("leapPath = '%s'" % os.path.join(leapSDKDir, 'lib'))
+         
+    def _writePymolrc(self):
+        pymolrcPath = os.path.expanduser('~/.pymolrc')
+        ocumolStartBumper = '#'*4 + 'START_OCUMOL_PLUGIN' + '#'*4
+        ocumolEndBumper = '#'*4 + 'END_OCUMOL_PLUGIN' + '#'*4
+        with open(os.path.join(thisScriptDir, 'pymolrc'), 'r') as f:
+            ocumolPluginTxt = f.read()
+        if not ReplaceBtwnLines(pymolrcPath, ocumolStartBumper, ocumolEndBumper, ocumolPluginTxt):
+            AppendBlockToFile(pymolrcPath, ocumolStartBumper, ocumolEndBumper, ocumolPluginTxt)
+
+class CustomDevelopCommand(CustomSetupCommand, develop):
+    def run(self):
+        CustomSetupCommand.run(self)
+        develop.run(self)
+
+class CustomInstallCommand(CustomSetupCommand, install):
+    def run(self):
+        CustomSetupCommand.run(self)
+        install.run(self)
 
 setup(
-    name = "ocudump",
-    package_dir = {'': 'build/src/cython'},
-    data_files = [(get_python_lib(), glob.glob('build/src/cython/*.so'))],
     author = 'Max Klein',
+    cmdclass = {'develop': CustomDevelopCommand,
+                'egg_info': CustomEggInfoCommand,
+                'install': CustomInstallCommand},
+    data_files = [(get_python_lib(), glob.glob('build/src/cython/*.so'))],
     description = 'lightweight library for dumping out tracking data from an Oculus Rift',
     license = 'UIOSL',
+    name = "ocudump",
+    package_dir = {'': 'build/src/cython'},
     zip_safe = False,
     )
--- utils.py
+++ utils.py
@@ -1,7 +1,17 @@
-from distutils.errors import DistutilsOptionError
+# aspects of this build system are based on https://github.com/PySide/pyside-setup
+from distutils import log
+from distutils.errors import DistutilsExecError, DistutilsOptionError, DistutilsSetupError
+from distutils.spawn import spawn
 import os,sys
+import struct
+import subprocess
 
-def option_value(name):
+import popenasync
+
+def IsPython64Bit():
+    return (struct.calcsize("P") == 8)
+
+def OptionValue(name):
     for index, option in enumerate(sys.argv):
         if option == '--' + name:
             if index+1 >= len(sys.argv):
@@ -16,3 +26,65 @@ def option_value(name):
             return value
     env_val = os.getenv(name.upper().replace('-', '_'))
     return env_val
+
+def RunProcess(args, cwd=None, initial_env=None):
+    def _log(buffer, checkNewLine=False):
+        endsWithNewLine = False
+        if buffer.endswith('\n'):
+            endsWithNewLine = True
+        if checkNewLine and buffer.find('\n') == -1:
+            return buffer
+        lines = buffer.splitlines()
+        buffer = ''
+        if checkNewLine and not endsWithNewLine:
+            buffer = lines[-1]
+            lines = lines[:-1]
+        for line in lines:
+            log.info(line.rstrip('\r'))
+        return buffer
+
+    _log("Running process: {0}".format(" ".join([(" " in x and '"{0}"'.format(x) or x) for x in args])))
+
+#     if sys.platform != "win32":
+#         try:
+#             spawn(args)
+#             return 0
+#         except DistutilsExecError:
+#             return -1
+
+    shell = False
+    if sys.platform == "win32":
+        shell = True
+
+    if initial_env is None:
+        initial_env = os.environ
+
+    proc = popenasync.Popen(args,
+        cwd = cwd,
+        stdin = subprocess.PIPE,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.STDOUT,
+        universal_newlines = 1,
+        shell = shell,
+        env = initial_env)
+
+    log_buffer = None;
+    while proc.poll() is None:
+        log_buffer = _log(proc.read_async(wait=0.1, e=0))
+    if log_buffer:
+        _log(log_buffer)
+
+    proc.wait()
+    return proc.returncode
+
+def WrappedMkdir(d, mode=0755):
+    try:
+        os.mkdir(d, mode)
+    except OSError:
+        pass
+    
+def WrappedMakedirs(d, mode=0755):
+    try:
+        os.makedirs(d, mode)
+    except OSError:
+        pass
